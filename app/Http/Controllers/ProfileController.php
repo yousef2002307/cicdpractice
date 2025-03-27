02699afc197c3d54cc19c2864b4c45ac194e5ba0
MAnwar <MAnwar@aas.com.sa>
2025-03-28 01:22:27 +0200

    example 01

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -14,7 +14,7 @@ class ProfileController extends Controller
     /**
      * Display the user's profile form.
      */
-    public function edit(Request $request): View
+    public function edit(Request $request): View 
     {
         return view('profile.edit', [
             'user' => $request->user(),
