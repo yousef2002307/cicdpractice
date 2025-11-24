f8b504226210169aa3a54628c1dc345eb9e06063
yousef2002307 <85695400+yousef2002307@users.noreply.github.com>
2025-11-24 09:54:23 +0200

    added all

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
