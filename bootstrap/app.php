73d44c5a4065c2b58a648a50ca2965d7e610e33c
yousef2002307 <85695400+yousef2002307@users.noreply.github.com>
2025-12-10 09:31:52 +0200

    feat: update questions with option emdpoint wit translations to  endpoint

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'webhook' => \Spatie\WebhookClient\Http\Middleware\VerifySignature::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
