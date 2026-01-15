1c489995a3a8e2c412ec0a5b1d0ba81a4ed54f67
yousef2002307 <85695400+yousef2002307@users.noreply.github.com>
2026-01-15 09:58:25 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard2', function () {
+Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
